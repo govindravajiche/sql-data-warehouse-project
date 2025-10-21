89396a053d48fbb43296a8bd6708957ae64a2186
Govind Ravajiche <101990964+govindravajiche@users.noreply.github.com>
2025-10-21 23:56:02 +0300

    Update proc_load_bronze.sql

--- scripts/proc_load_bronze.sql
+++ scripts/proc_load_bronze.sql
@@ -8,6 +8,8 @@ Script Purpose:
     - Truncates the bronze tables before loading data.
     - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
+
+
 Parameters:
     None. 
 	  This stored procedure does not accept any parameters or return any values.
@@ -139,3 +141,104 @@ BEGIN
 		PRINT '=========================================='
 	END CATCH
 END
+
+
+/*
+==========================================================================================================
+
+==========================================================================================================
+Script Purpose:
+==========================================================================================================
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+
+	PRINT '=============================================================================================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '=============================================================================================';
+
+	PRINT '=============================================================================================';
+	PRINT 'Loading CRM Table';
+	PRINT '=============================================================================================';
+
+	PRINT '>> Truncating Table: bronze.crm_cust_info';
+	TRUNCATE TABLE bronze.crm_cust_info;
+
+	PRINT '>> Inserting Data into : bronze.crm_cust_info';
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\Dell\Desktop\Data Engg\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>> Truncating Table: bronze.crm_prd_info';
+	TRUNCATE TABLE bronze.crm_prd_info;
+
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\Dell\Desktop\Data Engg\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>> Truncating Table: bronze.crm_sales_details';
+	TRUNCATE TABLE bronze.crm_sales_details;
+
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\Dell\Desktop\Data Engg\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>> Truncating Table: bronze.erp_cust_az12';
+	TRUNCATE TABLE bronze.erp_cust_az12;
+
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\Dell\Desktop\Data Engg\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>> Truncating Table: bronze.erp_loc_a101';
+	TRUNCATE TABLE bronze.erp_loc_a101;
+
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\Dell\Desktop\Data Engg\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\Dell\Desktop\Data Engg\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	PRINT 'Data loaded successfully into bronze layer.'
+
+		END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
+
+
+
+
+
+
+
